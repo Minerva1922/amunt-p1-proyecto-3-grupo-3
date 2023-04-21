4e32aab1e5d43d7b6668a2c2d0c60cedd64141f4
Minerva1922 <minervapv83@gmail.com>
2023-04-21 11:32:24 +0200

    commit inicial

---
+++ src/main/kotlin/com/example/proyecto4/Proyecto4Application.kt
@@ -0,0 +1,11 @@
+package com.example.proyecto4
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Proyecto4Application
+
+fun main(args: Array<String>) {
+    runApplication<Proyecto4Application>(*args)
+}
---
+++ src/test/kotlin/com/example/proyecto4/Proyecto4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.proyecto4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Proyecto4ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
