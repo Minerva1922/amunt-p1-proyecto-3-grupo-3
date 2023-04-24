ba4c3bec7b8f7786a3bafb4ff3c27c052f127766
IrinaSiokhina <irinabarcelona2807@gmail.com>
2023-04-24 14:32:11 +0200

    Lista de peliculas

--- src/main/kotlin/com/example/proyecto4/repositories/DataLoader.kt
+++ src/main/kotlin/com/example/proyecto4/repositories/DataLoader.kt
@@ -8,14 +8,14 @@ class DataLoader(private val movieRepository: MovieRepository) {
     @PostConstruct
     fun load() {
         val movies = listOf(
-            Movie("p","url de imagen", "spielberg", 1994,"lalalal"),
-            Movie("peli1","url de imagen", "spielberg", 1994,"lalalal"),
-            Movie("peli1","url de imagen", "spielberg", 1994,"lalalal"),
-            Movie("peli1","url de imagen", "spielberg", 1994,"lalalal"),
-            Movie("peli1","url de imagen", "spielberg", 1994,"lalalal"),
-            Movie("peli1","url de imagen", "spielberg", 1994,"lalalal"),
-            Movie("peli1","url de imagen", "spielberg", 1994,"lalalal"),
-            Movie("peli1","url de imagen", "spielberg", 1994,"lalalal")
+            Movie("Ratatouille","url de imagen", "Brad Bird", 2007,"lalalal"),
+            Movie("Jurassic Park","url de imagen", "Steven Spielberg", 1993,"lalalal"),
+            Movie("Cruella","url de imagen", "Craig Gillespie", 2021,"lalalal"),
+            Movie("Mean Girls","url de imagen", "Mark Waters", 2004,"lalalal"),
+            Movie("Lady Bird","url de imagen", "Greta Gerwig", 2017,"lalalal"),
+            Movie("Suffragette","url de imagen", "Sarah Gavron", 2015,"lalalal"),
+            Movie("On the basis of sex","url de imagen", "Mimi Leder", 2018,"lalalal"),
+            Movie("La vie est belle","url de imagen", "Roberto Benigni", 1999,"lalalal")
 
         )
         movieRepository.saveAll(movies)
