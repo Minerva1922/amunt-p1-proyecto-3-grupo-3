cb747e0ebf9c88fc7e10240c09976baba2cd8dfa
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-04-25 12:08:45 +0200

    get post put delete

--- src/main/kotlin/com/example/proyecto4/controllers/MovieController.kt
+++ src/main/kotlin/com/example/proyecto4/controllers/MovieController.kt
@@ -14,28 +14,28 @@ class MovieController(@Autowired private val movieRepository: MovieRepository) {
         return movieRepository.findAll()
     }
 
-//    @GetMapping("/movies/{id}")
-//    fun findMovie(@PathVariable id: Long): Movie? {
-//        return movieRepository.findById(id).orElseThrow { MovieNotFoundException() }
-//    }
+    @GetMapping("/movies/{id}")
+    fun findMovie(@PathVariable id: Long): Movie {
+        return movieRepository.findById(id).orElseThrow { MovieNotFoundException() }
+    }
+
+    @PostMapping("/movies")
+    fun addMovie(@RequestBody movie: Movie): Movie {
+        return movieRepository.save(movie)
+    }
 //
-//    @PostMapping("/movies")
-//    fun addMovie(@RequestBody movie: Movie): Movie? {
-//        return movieRepository.save(movie)
-//    }
+    @DeleteMapping("/movies/{id}")
+    fun deleteMovieById(@PathVariable id: Long): Movie {
+        val movie: Movie = movieRepository.findById(id).orElseThrow { MovieNotFoundException() }
+        movieRepository.deleteById(id)
+        return movie
+    }
 //
-//    @DeleteMapping("/movies/{id}")
-//    fun deleteMovieById(@PathVariable id: Long): Movie? {
-//        val movie: Movie = movieRepository.findById(id).orElseThrow { MovieNotFoundException() }
-//        movieRepository.deleteById(id)
-//        return movie
-//    }
-//
-//    @PutMapping("/movies")
-//    fun updateCoderById(@RequestBody movie:  Movie): Movie? {
-//        movie.id?.let { movieRepository.findById(it).orElseThrow { MovieNotFoundException() } }
-//        return movieRepository.save(movie)
-//    }
+    @PutMapping("/movies")
+    fun updateMovieById(@RequestBody movie:  Movie): Movie {
+        movie.id?.let { movieRepository.findById(it).orElseThrow { MovieNotFoundException() } }
+        return movieRepository.save(movie)
+    }
 }
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "movie not found")
