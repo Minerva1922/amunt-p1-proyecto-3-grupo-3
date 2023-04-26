548d505c8b49c278fe5e5c2c4ab14f6100e73ae1
Minerva1922 <minervapv83@gmail.com>
2023-04-26 10:52:45 +0200

    cambios coder x movies

--- src/test/kotlin/com/example/proyecto4/Proyecto4ApplicationTests.kt
+++ src/test/kotlin/com/example/proyecto4/Proyecto4ApplicationTests.kt
@@ -1,5 +1,7 @@
 package com.example.proyecto4
 
+import com.example.proyecto4.repositories.Movie
+import com.example.proyecto4.repositories.MovieRepository
 import org.hamcrest.MatcherAssert
 import org.hamcrest.Matchers
 import org.junit.jupiter.api.AfterEach
@@ -14,28 +16,28 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @SpringBootTest
-classes = arrayOf(proyecto4Application::class),
+class = arrayOf(proyecto4Application::class),
 webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-class Proyecto4ApplicationTests (@Autowired val mockMvc: MockMvc) {
+class Proyecto4ApplicationTests(@Autowired val mockMvc: MockMvc) {
 
-@Autowired
-    private lateinit var coderRepository: MovieRepository
+    @Autowired
+    private lateinit var movieRepository: MovieRepository
 
     @AfterEach
     fun tearDown() {
-        coderRepository.deleteAll()
+        movieRepository.deleteAll()
     }
 
     @Test
     @Throws(Exception::class)
-    fun `returns the existing coders`() {
-        addTestCoders()
-        mockMvc.perform(MockMvcRequestBuilders.get("/coders"))
+    fun `returns the existing movie`() {
+        addTestMovies()
+        mockMvc.perform(MockMvcRequestBuilders.get("/movies"))
             .andExpect(MockMvcResultMatchers.status().isOk)
             .andExpect(MockMvcResultMatchers.jsonPath("$[*]", Matchers.hasSize<Int>(3)))
-            .andExpect(MockMvcResultMatchers.jsonPath("$[0].name", Matchers.equalTo("Yeraldin")))
-            .andExpect(MockMvcResultMatchers.jsonPath("$[0].favouriteLanguage", Matchers.equalTo("Java")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[0].title", Matchers.equalTo("Ratatouille")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[0].director", Matchers.equalTo("Java")))
             .andExpect(MockMvcResultMatchers.jsonPath("$[1].name", Matchers.equalTo("Marta")))
             .andExpect(MockMvcResultMatchers.jsonPath("$[1].favouriteLanguage", Matchers.equalTo("Kotlin")))
             .andExpect(MockMvcResultMatchers.jsonPath("$[2].name", Matchers.equalTo("Daniela")))
@@ -43,15 +45,19 @@ class Proyecto4ApplicationTests (@Autowired val mockMvc: MockMvc) {
             .andDo(MockMvcResultHandlers.print())
     }
 
+    private fun addTestMovies() {
+        TODO("Not yet implemented")
+    }
+
     @Test
     @Throws(Exception::class)
-    fun `allows to create a new coder`() {
+    fun `allows to create a new movie`() {
         mockMvc.perform(
-            MockMvcRequestBuilders.post("/coders")
+            MockMvcRequestBuilders.post("/movie")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\"name\": \"Laura\", \"favouriteLanguage\": \"PHP\" }")
         ).andExpect(MockMvcResultMatchers.status().isOk)
-        val coders: List<Coder> = coderRepository.findAll()
+        val coders: List<Movie> = movieRepository.findAll()
         MatcherAssert.assertThat(
             coders, Matchers.contains(
                 Matchers.allOf(
@@ -64,30 +70,30 @@ class Proyecto4ApplicationTests (@Autowired val mockMvc: MockMvc) {
 
     @Test
     @Throws(Exception::class)
-    fun `allows to find a coder by id`() {
-        val coder: Coder = coderRepository.save(Coder("Marta", "Kotlin"))
-        mockMvc.perform(MockMvcRequestBuilders.get("/coders/" + coder.id))
+    fun `allows to find a movie by id`() {
+        val movie: Movie = movieRepository.save(Movie("", ""))
+        mockMvc.perform(MockMvcRequestBuilders.get("/movies/" + movie.id))
             .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.equalTo("Marta")))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.favouriteLanguage", Matchers.equalTo("Kotlin")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.equalTo("")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.favouriteLanguage", Matchers.equalTo("")))
     }
 
     @Test
     @Throws(Exception::class)
     fun `returns an error if trying to get a coder that does not exist`() {
-        mockMvc.perform(MockMvcRequestBuilders.get("/coders/1"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/movies/1"))
             .andExpect(MockMvcResultMatchers.status().isNotFound())
     }
 
     @Test
     @Throws(Exception::class)
     fun `allows to delete a coder by id`() {
-        val coder: Coder = coderRepository.save(Coder("Marta", "Kotlin"))
+        val movie: Movie = movieRepository.save(Movie("Marta", "Kotlin"))
         mockMvc.perform(MockMvcRequestBuilders.delete("/coders/" + coder.id))
             .andExpect(MockMvcResultMatchers.status().isOk)
-        val coders: List<Coder> = coderRepository.findAll()
+        val movies: List<Movie> = movieRepository.findAll()
         MatcherAssert.assertThat(
-            coders, Matchers.not(
+            movies, Matchers.not(
                 Matchers.contains(
                     Matchers.allOf(
                         Matchers.hasProperty("name", Matchers.`is`("Marta")),
@@ -100,45 +106,45 @@ class Proyecto4ApplicationTests (@Autowired val mockMvc: MockMvc) {
 
     @Test
     @Throws(Exception::class)
-    fun `returns an error if trying to delete a coder that does not exist`() {
-        mockMvc.perform(MockMvcRequestBuilders.delete("/coders/1"))
+    fun `returns an error if trying to delete a movie that does not exist`() {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/movies/1"))
             .andExpect(MockMvcResultMatchers.status().isNotFound())
     }
 
 
     @Test
     @Throws(Exception::class)
-    fun `allows to modify a coder`() {
-        val coder: Coder = coderRepository.save(Coder("Yeraldin", "Java"))
+    fun `allows to modify a movie`() {
+        val coder: Movie = movieRepository.save(Movie("Yeraldin", "Java"))
         mockMvc.perform(
-            MockMvcRequestBuilders.put("/coders")
+            MockMvcRequestBuilders.put("/movies")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\"id\": \"" + coder.id + "\", \"name\": \"Yeraldin\", \"favouriteLanguage\": \"Ruby\" }")
         ).andExpect(MockMvcResultMatchers.status().isOk)
-        val coders: List<Coder> = coderRepository.findAll()
-        MatcherAssert.assertThat(coders, Matchers.hasSize(1))
-        MatcherAssert.assertThat(coders[0].name, Matchers.equalTo("Yeraldin"))
-        MatcherAssert.assertThat(coders[0].favouriteLanguage, Matchers.equalTo("Ruby"))
+        val movie: List<Movie> = movieRepository.findAll()
+        MatcherAssert.assertThat(Movie, Matchers.hasSize(1))
+        MatcherAssert.assertThat(Movie[0].name, Matchers.equalTo("Yeraldin"))
+        MatcherAssert.assertThat(Movie[0].favouriteLanguage, Matchers.equalTo("Ruby"))
     }
 
     @Test
     @Throws(Exception::class)
-    fun `returns an error when trying to modify a coder that does not exist`() {
-        addTestCoders()
+    fun `returns an error when trying to modify a movie that does not exist`() {
+        addTestMovies()
         mockMvc.perform(
-            MockMvcRequestBuilders.put("/coders")
+            MockMvcRequestBuilders.put("/movies")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\"id\": \"" + -1 + "\", \"name\": \"Pepita\", \"favouriteLanguage\": \"C++\" }")
         ).andExpect(MockMvcResultMatchers.status().isNotFound())
     }
 
-    private fun addTestCoders() {
-        val coders: List<Coder> = listOf(
-            Coder("Yeraldin", "Java"),
-            Coder("Marta", "Kotlin"),
-            Coder("Daniela", "Python")
+    private fun addTestMovie() {
+        val movies: List<Movie> = listOf(
+            Movie("", ""),
+            Movie("", ""),
+            Movie("", "")
         )
-        coders.forEach(coderRepository::save)
+        movies.forEach { movieRepository.save(it) }
     }
 
 
